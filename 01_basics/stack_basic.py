1fda368ee0b754c7e288574c9a4074db9c9b0e83
Narsing Narvate <narvatenarsing8@gmail.com>
2026-04-15 22:13:38 +0530

    Create stack_basic.py
    Added basic stack implementation using list

---
+++ 01_basics/stack_basic.py
@@ -0,0 +1,18 @@
+# Stack Implementation using List
+
+stack = []
+
+# Push operation
+stack.append(10)
+stack.append(20)
+stack.append(30)
+
+print("Stack after push:", stack)
+
+# Pop operation
+stack.pop()
+
+print("Stack after pop:", stack)
+
+# Peek
+print("Top element:", stack[-1])
